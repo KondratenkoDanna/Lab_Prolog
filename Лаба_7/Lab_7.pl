50053262618170ea80b05f5caa3d5bd13a3ac013
Kondratenko Danna <72597800+KondratenkoDanna@users.noreply.github.com>
2021-03-14 11:51:40 +0300

    Task 2.

--- Лаба_7/Lab_7.pl
+++ Лаба_7/Lab_7.pl
@@ -12,14 +12,26 @@ r_str(X,A,B,N,K):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1).
 in_list([El|_],El).
 in_list([_|T],El):-in_list(T,El).
 
+append1([],List,List):-!.
+append1([H1|T1],List,[H1|T3]):-append(T1,List,T3).
+
+reverse(A, Z) :- reverse(A,Z,[]).
+reverse([],Z,Z).
+reverse([H|T],Z,Acc) :- reverse(T,Z,[H|Acc]).
+
 		%___________1___________
 wr_str:-read_str(A,Length,0),write_str(A),write(","),write_str(A),write(","),
 												write_str(A),write(","),write(Length).
 
 		%___________2___________
+kol_w:-read_str(A,N,0),append1([32],A,A1),kol_words(A1,0,Kol),write(Kol).
+kol_words([H|[]],Kol,Kol):-!.
+kol_words([H1|[H2|T]],K,Kol):-(H1=32,H2\=32 -> K1 is K+1,kol_words([H2|T],K1,Kol);
+							  kol_words([H2|T],K,Kol)).
+
 		%___________3___________
 		%___________4___________
-		%___________5___________
+		%___________5___________   
 		%___________6___________
 		%___________7___________
 		%___________8___________
@@ -27,3 +39,12 @@ wr_str:-read_str(A,Length,0),write_str(A),write(","),write_str(A),write(","),
 		%___________10___________
 		%___________11___________
 		%___________12___________
+		%___________13___________
+		%___________14___________
+		%___________15___________
+		%___________16___________
+		%___________17___________
+		%___________18___________
+		%___________19___________
+		%___________20___________
+		%___________21___________
