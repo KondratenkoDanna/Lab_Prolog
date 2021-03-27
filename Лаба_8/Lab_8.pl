034e5408a95483c05ca02f2fb05a4b50d287b369
Kondratenko Danna <72597800+KondratenkoDanna@users.noreply.github.com>
2021-03-27 14:27:34 +0300

    Task 6.2.

--- Лаба_8/Lab_8.pl
+++ Лаба_8/Lab_8.pl
@@ -15,7 +15,7 @@ append1([H1|T1],List2,[H1|T3]):-append1(T1,List2,T3).
 read_list_str(List):-read_str(A,_,Flag),read_list_str([A],List,Flag).
 read_list_str(List,List,1):-!.
 read_list_str(Cur_list,List,0):-
-	read_str(A,N,Flag),append(Cur_list,[A],C_l),read_list_str(C_l,List,Flag).
+	read_str(A,_,Flag),append(Cur_list,[A],C_l),read_list_str(C_l,List,Flag).
 
 length_word([],0):-!.
 length_word([_|T],L):-length_word(T,L1),L is L1+1.
@@ -28,29 +28,29 @@ reverse([],Z,Z).
 reverse([H|T],Z,Acc) :- reverse(T,Z,[H|Acc]).
 
 		%______________1______________
-max_l_w:-see('c:/Users/danna/Desktop/output.txt'),read_list_str(List),seen,
-		 tell('c:/Users/danna/Desktop/input.txt'),max_l_w(List,0,K),write(K),told.
+max_l_w:-see('c:/Users/danna/Desktop/input.txt'),read_list_str(List),seen,
+		 tell('c:/Users/danna/Desktop/output.txt'),max_l_w(List,0,K),write(K),told.
 max_l_w([],K,K):-!.
 max_l_w([H|T],K,Kol):-length_word(H,L),(L>K -> K1 is L,max_l_w(T,K1,Kol);max_l_w(T,K,Kol)).
 
 		%______________2______________
-kol_st_no_zero:-see('c:/Users/danna/Desktop/output.txt'),read_list_str(List),seen,
-				tell('c:/Users/danna/Desktop/input.txt'),kol_st_no_zero(List,0,Kol),write(Kol),told.
+kol_st_no_zero:-see('c:/Users/danna/Desktop/input.txt'),read_list_str(List),seen,
+				tell('c:/Users/danna/Desktop/output.txt'),kol_st_no_zero(List,0,Kol),write(Kol),told.
 
 kol_st_no_zero([],K,K):-!.
 kol_st_no_zero([H|T],K,Kol):-not(in_list(H,32)),K1 is K+1,kol_st_no_zero(T,K1,Kol),!.
-kol_st_no_zero([H|T],K,Kol):-kol_st_no_zero(T,K,Kol),!.
+kol_st_no_zero([_|T],K,Kol):-kol_st_no_zero(T,K,Kol),!.
 
 		%______________3______________
-sr_A:-see('c:/Users/danna/Desktop/output.txt'),read_list_str(List),seen,
-	  tell('c:/Users/danna/Desktop/input.txt'),kol_A_in_list(List,0,KA),
+sr_A:-see('c:/Users/danna/Desktop/input.txt'),read_list_str(List),seen,
+	  tell('c:/Users/danna/Desktop/output.txt'),kol_A_in_list(List,0,KA),
 	  sr_kol_A(List,KA,SrA),write_stroka_srA(List,SrA),told.
 sr_kol_A([H|T],KolA,SrA):-length_word([H|T],L),SrA is KolA div L.
 kol_A_in_list([],K,K):-!.
 kol_A_in_list([H|T],K,Kol):-kol_A(H,0,KolA),K1 is K+KolA,kol_A_in_list(T,K1,Kol).
 write_stroka_srA([],_):-!.
 write_stroka_srA([H|T],SrKolA):-kol_A(H,0,K),K>SrKolA,write_str(H),nl,write_stroka_srA(T,SrKolA),!.
-write_stroka_srA([H|T],SrKolA):-write_stroka_srA(T,SrKolA),!.
+write_stroka_srA([_|T],SrKolA):-write_stroka_srA(T,SrKolA),!.
 
 		%______________4______________
 list_words(A,LW):-append1([32],A,A1),reverse(A1,AR),list_words(AR,[],LW,[]).
@@ -58,35 +58,48 @@ list_words([],LW,LW,_):-!.
 list_words([H|T],LW,LWN,W):-((H=32;H=10) -> append([W],LW,LW1),list_words(T,LW1,LWN,[]);
 						 append1([H],W,W1),list_words(T,LW,LWN,W1)).
 list_words_all_file([],ListAllWord,ListAllWord):-!.
-list_words_all_file(Stroka,List,ListAllWord):-list_words(Stroka,LW),append1(List,LW,List1),
-										list_words_all_file(T,List1,ListAllWord).
+list_words_all_file(Stroka,List,ListAllWord):-list_words(Stroka,LW),append1(List,LW,ListAllWord).
 
 kol_repeat_in_list([H|T],X,K):-kol_repeat_in_list([H|T],X,0,K).
 kol_repeat_in_list([],_,Kol,Kol):-!.
 kol_repeat_in_list([H|T],X,K,Kol):-(H=X -> K1 is K+1,kol_repeat_in_list(T,X,K1,Kol);
 									kol_repeat_in_list(T,X,K,Kol)).
 
-often_word_in_list:-see('c:/Users/danna/Desktop/output.txt'),read_str(A,_,1),seen,
-					tell('c:/Users/danna/Desktop/input.txt'),list_words_all_file(A,[],ListAllWord),
+often_word_in_list:-see('c:/Users/danna/Desktop/input.txt'),read_str(A,_,1),seen,
+					tell('c:/Users/danna/Desktop/output.txt'),list_words_all_file(A,[],ListAllWord),
 					often_word_in_list(ListAllWord,_,Word,0,_),write_str(Word),told.
 often_word_in_list([],Word,Word,Kol,Kol):-!.
 often_word_in_list([H|T],W,Word,K,Kol):-kol_repeat_in_list([H|T],H,K1),(K1>K -> Kol1 = K1,W1=H,
 					    often_word_in_list(T,W1,Word,K1,Kol1);often_word_in_list(T,W,Word,K,Kol)).
 
 
-		%______________5______________
-no_repeat_str:-see('c:/Users/danna/Desktop/output.txt'),read_str(A,_,1),seen,
-	           tell('c:/Users/danna/Desktop/input.txt'),no_repeat_str(A),told.
-no_repeat_str([]):-!.
-no_repeat_str([H|T]):-kol_repeat_in_list([H|T],H,K),K>1,write_str(H),nl,no_repeat_str(T),!.
-no_repeat_str([H|T]):-no_repeat_str(T).
+		%______________5______________///////////////
+%no_repeat_str:-see('c:/Users/danna/Desktop/input.txt'),read_str(A,_,1),seen,
+%	           tell('c:/Users/danna/Desktop/output.txt'),list_words_all_file(A,[],ListWordAllFile),
+%			   no_repeat_str(A,ListWordAllFile),told.
+%no_repeat_str([]):-!.
+%no_repeat_str([H|T],LW):-list_words(H,[H1|T1]),kol_repeat_in_list([H|T],H,K),K>1,write_str(H),nl,no_repeat_str(T,LW),!.
+%no_repeat_str([H|T],LW):-no_repeat_str(T,LW).
 
-		%______________6.2______________
+%repeat_in_list([H|T],X):-repeat_in_list([H|T],X).
+%repeat_in_list([],_):-true,!.
+%repeat_in_list([H|T],X,K,Kol):-H=X,fail,!.
+%repeat_in_list([H|T],X,K,Kol):-repeat_in_list(T,X).
+%pr_in_list(ListW,[H|T]):-in_list().								
+
+		%______________6.2_____________
+sort_list:-see('c:/Users/danna/Desktop/input.txt'),read_str(A,_,1),seen,
+		   tell('c:/Users/danna/Desktop/output.txt'),sort_list(A),told.
+sort_list([_]):-write("Yes"),!.
+sort_list([H1,H2|T]):-H1<H2,!,sort_list([H2|T]).
+sort_list([_,_|_]):-write("No"),!.
+
+
 		%______________6.10______________
-kol_A:-see('c:/Users/danna/Desktop/output.txt'),read_str(St,_,1),seen,
-	   tell('c:/Users/danna/Desktop/input.txt'),kol_A(St,0,K),write(K),told.	
+kol_A:-see('c:/Users/danna/Desktop/input.txt'),read_str(St,_,1),seen,
+	   tell('c:/Users/danna/Desktop/output.txt'),kol_A(St,0,K),write(K),told.	
 kol_A([],K,K):-!.
 kol_A([H|T],K,Kol):-H=65,K1 is K+1,kol_A(T,K1,Kol),!.
-kol_A([H|T],K,Kol):-kol_A(T,K,Kol).
+kol_A([_|T],K,Kol):-kol_A(T,K,Kol).
 
 		%______________6.17______________
