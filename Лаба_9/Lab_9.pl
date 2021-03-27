278f694ece10908273c410ddd9dfdbcdd0b87dbf
Kondratenko Danna <72597800+KondratenkoDanna@users.noreply.github.com>
2021-03-27 23:13:56 +0300

    Task 1.

--- Лаба_9/Lab_9.pl
+++ Лаба_9/Lab_9.pl
@@ -8,48 +8,64 @@ append1([H1|T1],List2,[H1|T3]):-append1(T1,List2,T3).
 
 read_list_str(List):-read_str(A,_,Flag),read_list_str([A],List,Flag).
 read_list_str(List,List,1):-!.
-read_list_str(Cur_list,List,0):-
-	read_str(A,N,Flag),append(Cur_list,[A],C_l),read_list_str(C_l,List,Flag).
+read_list_str(Cur_list,List,0):-read_str(A,_,Flag),append(Cur_list,[A],C_l),
+								read_list_str(C_l,List,Flag).
 
 write_str([]):-!.
 write_str([H|Tail]):-put(H),write_str(Tail).
 
-in_list_exlude([El|T],El,T).
-in_list_exlude([H|T],El,[H|Tail]):-in_list_exlude(T,El,Tail).
+x_l_w:-
+		 tell('c:/Users/danna/Desktop/input.txt'),told.
 
 in_list([El|_],El).
 in_list([_|T],El):-in_list(T,El).
 
-x_l_w:-see('c:/Users/danna/Desktop/output.txt'),read_list_str(List),seen,
-		 tell('c:/Users/danna/Desktop/input.txt'),b_a_r(List,2,[]),told.
+read_str(A,N):-get0(X),r_str(X,A,[],N,0).
+r_str(10,A,A,N,N):-!.
+r_str(X,A,B,N,K):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1).
 
 		%______________1______________
-build_all_razm:-see('c:/Users/danna/Desktop/output.txt'),read_str(List,N,1),seen,
-				tell('c:/Users/danna/Desktop/input.txt'),b_a_r(List,2,[]),put(100),told.
+build_all_razm_p:-read_str(A,_),read(K),tell('c:/Users/danna/Desktop/output.txt'),
+				  not(b_a_rp(A,K,[])),told.
 		
-b_a_r(A,0,Perm1):-write_str(Perm1),nl,!,fail.
-b_a_r(A,N,Perm):-in_list_exlude(A,El,NA),N1 is N-1,b_a_r(NA,N1,[El|Perm]).
-
-build_all_razm_p:-read_str(A,N,0),read(K),b_a_rp(A,K,[]).
-		
-b_a_rp(A,0,Perm1):-write_str(Perm1),nl,!,fail.
+b_a_rp(_,0,Perm1):-write_str(Perm1),nl,!,false.
 b_a_rp(A,N,Perm):-in_list(A,El),N1 is N-1,b_a_rp(A,N1,[El|Perm]).
+
+build_all_perm:-read_str(A,_),tell('c:/Users/danna/Desktop/output.txt'),
+				not(b_a_p(A,[])),told.
+
+in_list_exlude([El|T],El,T).
+in_list_exlude([H|T],El,[H|Tail]):-in_list_exlude(T,El,Tail).
+
+b_a_p([],Perm1):-write_str(Perm1),nl,!,fail.
+b_a_p(A,Perm):-in_list_exlude(A,El,A1),b_a_p(A1,[El|Perm]).
+
+build_all_razm:-read_str(A,_),read(K),tell('c:/Users/danna/Desktop/output.txt'),
+				not(b_a_r(A,K,[])),told.
+		
+b_a_r(_,0,Perm1):-write_str(Perm1),nl,!,fail.
+b_a_r(A,N,Perm):-in_list_exlude(A,El,A1),N1 is N-1,b_a_r(A1,N1,[El|Perm]).
 
 sub_set([],[]).
 sub_set([H|Sub_set],[H|Set]):-sub_set(Sub_set,Set).
-sub_set(Sub_set,[H|Set]):-sub_set(Sub_set,Set).
-pr_subset:-read_str(A,N),sub_set(B,A),write_str(B),nl,fail.
-
+sub_set(Sub_set,[_|Set]):-sub_set(Sub_set,Set).
+pr_subset(A):-sub_set(B,A),write_str(B),nl,fail.
+wr_file_subset:-read_str(A,_),tell('c:/Users/danna/Desktop/output.txt'),
+				not(pr_subset(A)),told.
 
 sochet([],0,_):-!.
 sochet([H|Sub_set],K,[H|Set]):-K1 is K-1,sochet(Sub_set,K1,Set).
 sochet(Sub_set,K,[_|Set]):-sochet(Sub_set,K,Set).
-pr_sochet:-read_str(A,N),read(K),sochet(B,K,A),write_str(B),nl,fail.
+pr_sochet(A,K):-sochet(B,K,A),write_str(B),nl,fail.
+wr_file_sochet:-read_str(A,_),read(K),tell('c:/Users/danna/Desktop/output.txt'),
+				not(pr_sochet(A,K)),told.
 
 sochet_p([],0,_):-!.
 sochet_p([H|Sub_set],K,[H|Set]):-K1 is K-1,sochet_p(Sub_set,K1,[H|Set]).
 sochet_p(Sub_set,K,[_|Set]):-sochet_p(Sub_set,K,Set).
-pr_sochet_p:-read_str(A,N),read(K),sochet_p(B,K,A),write_str(B),nl,fail.
+pr_sochet_p(A,K):-sochet_p(B,K,A),write_str(B),nl,fail.
+wr_file_sochet_p:-read_str(A,_),read(K),tell('c:/Users/danna/Desktop/output.txt'),
+				not(pr_sochet_p(A,K)),told.
 
 		%______________2______________
 		%______________3______________
@@ -61,3 +77,5 @@ pr_sochet_p:-read_str(A,N),read(K),sochet_p(B,K,A),write_str(B),nl,fail.
 		%______________9______________
 		%______________10______________
 		%______________11______________
+
+n:-nodebug.
